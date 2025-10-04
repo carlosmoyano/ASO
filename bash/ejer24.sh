7f2273fba3a7ee69bb8204261271e7f3f5b12494
Carlos Moyano <cmoyanomoreno@gmail.com>
2025-10-04 12:40:13 +0200

    Subida últimos ejercicios

--- bash/ejer24.sh
+++ bash/ejer24.sh
@@ -4,14 +4,20 @@ echo "Listar ruta"
 echo "+++++++++++"
 
 
+totalficheros=0
+totaldirectorios=0
+
 if [[ ! -d "$1" ]]; then
 	echo "Lo que has pasado no es un directorio"
 else 
 	for i in "$1"/*; do
 		if [[ -f $i ]]; then
 			echo " $i --> es un fichero"
+			((totalficheros++))
 		else
 			echo " $i --> es un directorio"
+			((totaldirectorios++))
 		fi
 	done
+	echo -e "\n\nTotal de ficheros --> $totalficheros \nTotal directorios --> $totaldirectorios "
 fi
