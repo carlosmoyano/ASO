987d94c985e1792fb0a5ad4b2431574957739dc6
Carlos Moyano <79198679+carlosmoyano@users.noreply.github.com>
2026-01-22 12:54:18 +0100

    Add files via upload
    ejercicios práctica 2 del libro digital

---
+++ PowerShell/p2ej2.ps1
@@ -0,0 +1,8 @@
+﻿[int] $num1 = Read-Host "¿Primér número? "
+[int] $num2 = Read-Host "¿Segúndo número número? "
+
+Write-Host "Suma => $($num1 + $num2)"
+Write-Host "Resta => $($num1 - $num2)"
+Write-Host "Multiplicación => $($num1 * $num2)"
+Write-Host "División => $($num1 / $num2)"
+Write-Host "Resto => $($num1 % $num2)"
---
+++ PowerShell/p2ej3.ps1
@@ -0,0 +1,7 @@
+﻿[int] $numHoras = Read-Host "¿Número de horas trabajadas? "
+[int] $salarioHora = Read-Host "¿Coste por hora? "
+
+Write-Host "**********************************"
+
+Write-Host "Salario que deberías cobrar ==> $($numHoras * $salarioHora) euros "
+Write-Host "Lo que vás a cobrar ==> $(($numHoras * $salarioHora) / 1.75 ) euros.  Hay recortes.... a joderse :)"
