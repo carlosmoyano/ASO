4aff9e3e6cd20443e979bc6fe8e671d5971d59f3
Carlos Moyano <79198679+carlosmoyano@users.noreply.github.com>
2026-01-24 11:13:22 +0100

    Add files via upload

---
+++ PowerShell/ping.ps1
@@ -0,0 +1,13 @@
+﻿function TestPing ($Address) {
+
+    $Result = Test-Connection -ComputerName $Address -ErrorAction SilentlyContinue
+
+    if ($? -eq $true) { 
+      #  Write-Host "Ping correcto a $Address "
+        Write-Output "Ping correcto a $Address "
+    } else {
+        Write-Host "Ping no correcto a $Address "
+    }
+}
+
+TestPing "192.168.1.1"
